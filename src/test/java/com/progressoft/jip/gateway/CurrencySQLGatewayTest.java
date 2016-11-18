dd93f68423a7e8064a035cfbd416ba29f193f8f5
Saleem <saleemkhair@gmail.com>
2016-11-18 17:54:38 +0200

    A test suite for CurrencySQLGateway
    -It uses a sorted sequence of tests to maintain data persistence and
    avoid false passing tests
    
    -the '_#_method();' naming convention is for the purpose of preserving
    sort

---
+++ src/test/java/com/progressoft/jip/gateway/CurrencySQLGatewayTest.java
@@ -0,0 +1,123 @@
+package com.progressoft.jip.gateway;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runners.MethodSorters;
+
+import com.progressoft.jip.datastructures.Currency;
+import com.progressoft.jip.gateway.CurrencyGatewaySQL.DuplicateCurrencyCodeException;
+import com.progressoft.jip.gateway.CurrencyGatewaySQL.EmptyResultSetException;
+import com.progressoft.jip.gateway.CurrencyGatewaySQL.ShortCurrencyCodeException;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class CurrencySQLGatewayTest {
+	
+	
+	
+
+	@Rule
+	public final ExpectedException expectedException = ExpectedException.none();
+
+	
+	
+	private static List<Currency> currencies = new ArrayList<>();
+	private static final Currency newCurrency = new Currency();
+	
+	{
+		
+		newCurrency.setCurrencyCode("SAR");
+		newCurrency.setCurrencyRate(0.54);
+		newCurrency.setCurrencyDescription("Saudi Arabian Ryal");
+	}
+	
+	private BasicDataSource dataSource = new BasicDataSource();
+	private CurrencyGateway currencyGatewaty;
+
+
+
+	
+
+	@Before
+	public void setUp() {
+
+		dataSource.setUrl("jdbc:mysql://localhost/payment_system");
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		currencyGatewaty = new CurrencyGatewaySQL(dataSource);
+	}
+	
+	@Test
+	public void _01_loadCurrencies() {
+		Iterable<Currency> currencies = currencyGatewaty.loadCurrencies();
+		currencies.forEach(c -> CurrencySQLGatewayTest.currencies.add(c));
+		assertTrue(currencies.iterator().hasNext());
+
+	}
+	
+	@Test
+	public void _02_getValidCurrencyByCode() {
+		Currency validCurrency = currencies.get(0);
+		Currency currency = currencyGatewaty.getCurrencyByCode(validCurrency.getCurrencyCode());
+		assertNotNull(currency);
+	}
+	
+	@Test
+	public void _03_getInvalidCurrencyByCode(){
+		expectedException.expect(ShortCurrencyCodeException.class);
+		currencyGatewaty.getCurrencyByCode("J");
+		
+	}
+	
+	@Test
+	public void loadFromEmptyResultsSetTest(){
+		expectedException.expect(EmptyResultSetException.class);
+		currencyGatewaty.getCurrencyByCode("SOS");
+	}
+	
+	@Test
+	public void updateCurrencybyCode() {
+		double newRate = 4.3;
+		double maxDelta = 0;
+		Currency validCurrency = currencies.get(0);
+		currencyGatewaty.updateCurrencyRateByCode(validCurrency.getCurrencyCode(), newRate);
+		Currency updatedCurrency = currencyGatewaty.getCurrencyByCode(validCurrency.getCurrencyCode());
+		assertEquals(newRate, updatedCurrency.getCurrencyRate(),maxDelta);
+	}
+
+	@Test
+	public void createCurrencyTest(){
+		currencyGatewaty.createCurrency(newCurrency);
+		
+		Currency created = currencyGatewaty.getCurrencyByCode(newCurrency.getCurrencyCode());
+		assertNotNull(created);
+	}
+
+
+		
+	@Test 
+	public void _04_createDuplicateCurrency(){
+		expectedException.expect(DuplicateCurrencyCodeException.class);
+		currencyGatewaty.createCurrency(currencies.get(0));
+	}
+	
+	
+	@Test
+	public void deleteCurrencyTest(){
+		currencyGatewaty.deleteCurrency(newCurrency);
+		expectedException.expect(EmptyResultSetException.class);
+		Currency deleted = currencyGatewaty.getCurrencyByCode(newCurrency.getCurrencyCode());
+		assertNull(deleted);
+		
+	}
+}
