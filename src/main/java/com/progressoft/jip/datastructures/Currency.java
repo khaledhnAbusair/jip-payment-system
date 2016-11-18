7c0c020991c8c5e13d4edde77d06c2a71a5bca63
Saleem <saleemkhair@gmail.com>
2016-11-18 17:58:39 +0200

    currncy data structure

--- src/main/java/com/progressoft/jip/datastructures/Currency.java
+++ src/main/java/com/progressoft/jip/datastructures/Currency.java
@@ -1,19 +1,43 @@
 package com.progressoft.jip.datastructures;
 
+
+
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 @XStreamAlias("Currency")
 public class Currency {
 
-    public static final Currency JOD = new Currency(1.31);
+	private String currencyCode;
+	private double currencyRate;
+	private String currencyDescription;
+	//private java.util.Currency currency;
+	
+	
+	
+	
+	public String getCurrencyCode() {
+		
+		return currencyCode;
+	}
 
-    private Currency(double base) {
-	this.base = base;
-    }
+	public void setCurrencyCode(String currencyCode) {
+		this.currencyCode = currencyCode;
+	}
 
-    private double base;
-    
-    public double base(){
-	return base;
-    }
+	public double getCurrencyRate() {
+		return currencyRate;
+	}
+
+	public void setCurrencyRate(double currencyRate) {
+		this.currencyRate = currencyRate;
+	}
+
+	public String getCurrencyDescription() {
+		return currencyDescription;
+	}
+
+	public void setCurrencyDescription(String currencyDescription) {
+		this.currencyDescription = currencyDescription;
+	}
+
 }
