875a677c28792e386406222f843d81daa9f14bba
Ghadeer Alomari <ghadeeralomari3@gmail.com>
2016-11-20 17:08:30 +0200

    Ghadeer

--- src/main/java/com/progressoft/jip/gateway/MySQLPaymentPurposeGateway.java
+++ src/main/java/com/progressoft/jip/gateway/MySQLPaymentPurposeGateway.java
@@ -1,19 +1,24 @@
 package com.progressoft.jip.gateway;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 import javax.sql.DataSource;
 
 import com.progressoft.jip.datastructures.PaymentPurposeDataStructure;
 
-public class MySQLPaymentPurposeGateway {
+public class MySQLPaymentPurposeGateway implements PaymentPurposeGateway {
 
 	private static final int NAME_COLUMN = 2;
 	private static final int CODE_COLUMN = 1;
+	private static final int CODE_COLUMN_MAX_LENGTH = 10;
+	private static final int NAME_COLUMN_MAX_LENGTH = 45;
 	private Connection connection;
 
 	public MySQLPaymentPurposeGateway(DataSource dataSource) {
@@ -22,12 +27,87 @@ public class MySQLPaymentPurposeGateway {
 		initConnection(dataSource);
 	}
 
+	@Override
 	public PaymentPurposeDataStructure loadPaymentPurposeByCode(String code) {
 		if (Objects.isNull(code))
 			throw new NullPaymentPurposeCodeException();
 		if (code.isEmpty())
 			throw new EmptyPaymentPurposeCodeException();
 		return loadPaymentPurposeFromDatabaseByCode(code);
+	}
+
+	@Override
+	public void insertPaymentPurpose(PaymentPurposeDataStructure paymentPurposeDataStructure) {
+		if (paymentPurposeDataStructure.getCode().length() > CODE_COLUMN_MAX_LENGTH)
+			throw new DataExceedingCodeColumnLimitException();
+		if (paymentPurposeDataStructure.getName().length() > NAME_COLUMN_MAX_LENGTH)
+			throw new DataExceedingNameColumnLimitException();
+		if (isExistedPaymentPurposeCode(paymentPurposeDataStructure.getCode()))
+			throw new DuplicatePaymentPurposeCodeException();
+		try {
+			stringValuesPreparedStatement("insert into PAYMENT_SYSTEM.PAYMENT_PURPOSE (CODE,NAME) VALUES(?,?)",
+					paymentPurposeDataStructure.getCode(), paymentPurposeDataStructure.getName()).executeUpdate();
+		} catch (SQLException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@Override
+	public Collection<PaymentPurposeDataStructure> loadPaymentPurposes() {
+		List<PaymentPurposeDataStructure> list = new ArrayList<PaymentPurposeDataStructure>();
+		try {
+			ResultSet resultSet = stringValuesPreparedStatement("select * from PAYMENT_SYSTEM.PAYMENT_PURPOSE")
+					.executeQuery();
+			while (resultSet.next())
+				list.add(new PaymentPurposeDataStructure(resultSet.getString(CODE_COLUMN),
+						resultSet.getString(NAME_COLUMN)));
+			return new ArrayList<>(list);
+		} catch (SQLException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@Override
+	public void deletePaymentPurposeByCode(String code) {
+		if (Objects.isNull(code))
+			throw new NullPaymentPurposeCodeException();
+		if (code.isEmpty())
+			throw new EmptyPaymentPurposeCodeException();
+		try {
+			int effectedRows = stringValuesPreparedStatement(
+					"delete from PAYMENT_SYSTEM.PAYMENT_PURPOSE where CODE = ?", code).executeUpdate();
+			if (effectedRows == 0)
+				throw new NoneExistingPaymentPurposeException();
+		} catch (SQLException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@Override
+	public void updatePaymentPurposeName(String code, String newName) {
+		if (Objects.isNull(code))
+			throw new NullPaymentPurposeCodeException();
+		if (code.isEmpty())
+			throw new EmptyPaymentPurposeCodeException();
+		if (Objects.isNull(newName))
+			throw new NullPaymentPurposeNameException();
+		try {
+			int effectedRows = stringValuesPreparedStatement(
+					"UPDATE PAYMENT_SYSTEM.PAYMENT_PURPOSE SET NAME = ? WHERE CODE = ?", newName, code).executeUpdate();
+			if (effectedRows == 0)
+				throw new NoneExistingPaymentPurposeException();
+		} catch (SQLException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private boolean isExistedPaymentPurposeCode(String code) {
+		try {
+			loadPaymentPurposeByCode(code);
+			return true;
+		} catch (PaymentPurposeNotFoundException e) {
+			return false;
+		}
 	}
 
 	private void initConnection(DataSource dataSource) {
@@ -42,8 +122,8 @@ public class MySQLPaymentPurposeGateway {
 		try {
 			ResultSet resultSet = stringValuesPreparedStatement(
 					"select * from PAYMENT_SYSTEM.PAYMENT_PURPOSE WHERE CODE = ?", code).executeQuery();
-			if(isEmptyResultSet(resultSet))
-				throw new EmptyResultSetException();
+			if (isEmptyResultSet(resultSet))
+				throw new PaymentPurposeNotFoundException();
 			return new PaymentPurposeDataStructure(resultSet.getString(CODE_COLUMN), resultSet.getString(NAME_COLUMN));
 		} catch (SQLException e) {
 			throw new IllegalStateException(e);
@@ -54,7 +134,7 @@ public class MySQLPaymentPurposeGateway {
 		return !resultSet.next();
 	}
 
-	private PreparedStatement stringValuesPreparedStatement(String sql, String ... vals) {
+	private PreparedStatement stringValuesPreparedStatement(String sql, String... vals) {
 		try {
 			PreparedStatement prepareStatement = connection.prepareStatement(sql);
 			for (int i = 0; i < vals.length; ++i)
@@ -64,12 +144,28 @@ public class MySQLPaymentPurposeGateway {
 			throw new IllegalStateException(e);
 		}
 	}
-	
+
+	static class NoneExistingPaymentPurposeException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
+	static class DataExceedingNameColumnLimitException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
+	static class DataExceedingCodeColumnLimitException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
+	static class DuplicatePaymentPurposeCodeException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
 	static class NullDataSourceException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 	}
 
-	static class EmptyResultSetException extends RuntimeException {
+	static class PaymentPurposeNotFoundException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 	}
 
@@ -80,4 +176,13 @@ public class MySQLPaymentPurposeGateway {
 	static class NullPaymentPurposeCodeException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 	}
+
+	static class EmptyPaymentPurposeNameException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
+	static class NullPaymentPurposeNameException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
 }
--- src/test/java/com/progressoft/jip/gateway/MySQLPaymentPurposeGatewayTest.java
+++ src/test/java/com/progressoft/jip/gateway/MySQLPaymentPurposeGatewayTest.java
@@ -2,16 +2,19 @@ package com.progressoft.jip.gateway;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.progressoft.jip.datastructures.PaymentPurposeDataStructure;
+import com.progressoft.jip.gateway.MySQLPaymentPurposeGateway.NullPaymentPurposeCodeException;
 
 public class MySQLPaymentPurposeGatewayTest {
 
-	private MySQLPaymentPurposeGateway mySQLPaymentPurposeGateway;
+	private PaymentPurposeGateway mySQLPaymentPurposeGateway;
 	private BasicDataSource dataSource;
 
 	@Before
@@ -43,23 +46,84 @@ public class MySQLPaymentPurposeGatewayTest {
 	public void givenPaymentPurposeGateway_CallingLoadPaymentPurposeByCode_PassingExistingCode_ShouldReturnPaymentPurpose() {
 		assertEquals(mySQLPaymentPurposeGateway.loadPaymentPurposeByCode("SALA").getCode(), "SALA");
 	}
-	
-	@Test(expected = MySQLPaymentPurposeGateway.EmptyResultSetException.class)
+
+	@Test(expected = MySQLPaymentPurposeGateway.PaymentPurposeNotFoundException.class)
 	public void givenPaymentPurposeGateway_CallingLoadPaymentPurposeByCode_PassingNoneExistingCode() {
 		mySQLPaymentPurposeGateway.loadPaymentPurposeByCode("ggg");
 	}
 
-	@Ignore
-	@Test
-	public void givenPaymentPurposeGateway_CallingInsertPaymentPurpose_PassNewPaymentPurpose_ThenCallingLoadPaymentPurposeByCode_ShouldReturnThePurpose() {
-		PaymentPurposeDataStructure paymentPurposeDataStructure = new PaymentPurposeDataStructure("SALA", "SALARY");
-		// mySQLPaymentPurposeGateway.insertPaymentPurpose(paymentPurposeDataStructure);
+	@Test(expected = MySQLPaymentPurposeGateway.DataExceedingCodeColumnLimitException.class)
+	public void givenPaymentPurposeGateway_CallingInsertPaymentPurpose_PassPaymentPurposeExceedingPurposeCodeLengthLimit_ShouldThrowDataExceedingCodeColumnLimit() {
+		PaymentPurposeDataStructure paymentPurposeDataStructure = new PaymentPurposeDataStructure("GHAD11111112241274",
+				"GHADEER");
+		mySQLPaymentPurposeGateway.insertPaymentPurpose(paymentPurposeDataStructure);
 	}
 
-	@Ignore
+	@Test(expected = MySQLPaymentPurposeGateway.DataExceedingNameColumnLimitException.class)
+	public void givenPaymentPurposeGateway_CallingInsertPaymentPurpose_PassPaymentPurposeExceedingPurposeNameLengthLimit_ShouldThrowDataExceedingCodeColumnLimit() {
+		PaymentPurposeDataStructure paymentPurposeDataStructure = new PaymentPurposeDataStructure("GHAD3",
+				"GHADEER124GHADEER124GHADEER124GHADEER124452712");
+		mySQLPaymentPurposeGateway.insertPaymentPurpose(paymentPurposeDataStructure);
+	}
+
+	@Test(expected = MySQLPaymentPurposeGateway.DuplicatePaymentPurposeCodeException.class)
+	public void givenPaymentPurposeGateway_CallingInsertPaymentPurpose_PassPaymentPurposWithExistingCode_ShouldThrowDuplicatePaymentPurposeCode() {
+		PaymentPurposeDataStructure paymentPurposeDataStructure = new PaymentPurposeDataStructure("GHAD", "GHADEER");
+		mySQLPaymentPurposeGateway.insertPaymentPurpose(paymentPurposeDataStructure);
+	}
+
 	@Test
 	public void givenPaymentPurposeGateway_CallingLoadPaymentPurposes_ShouldRetunPaymentPurposes() {
-
+		 assertEquals(9,new ArrayList<PaymentPurposeDataStructure>(
+					mySQLPaymentPurposeGateway.loadPaymentPurposes()).size());
+	}
+	
+	@Test
+	public void givenPaymentPurposeGateway_CallingInsertPaymentPurpose_ThenCallingDeletePurPose_ShouldInsertThePurposeThenDeleteIt() {
+		mySQLPaymentPurposeGateway.insertPaymentPurpose(new PaymentPurposeDataStructure("AHM", "AHMAD"));
+		mySQLPaymentPurposeGateway.deletePaymentPurposeByCode("AHM");
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.NoneExistingPaymentPurposeException.class)
+	public void givenPaymentPurposeGateway_CallingDeletePaymentPurposeByCode_PassingNoneExistingCode_ShouldThrowNoneExistingPaymentPurpose() {
+		mySQLPaymentPurposeGateway.deletePaymentPurposeByCode("CDFVD");
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.NullPaymentPurposeCodeException.class)
+	public void givenPaymentPurposeGateway_CallingDeletePaymentPurposeByCode_PassingNullCode_ShouldThrowNullPaymentPurposeCode() {
+		mySQLPaymentPurposeGateway.deletePaymentPurposeByCode(null);
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.EmptyPaymentPurposeCodeException.class)
+	public void givenPaymentPurposeGateway_CallingDeletePaymentPurposeByCode_PassingEmptyCode_ShouldThrowNullPaymentPurposeCode() {
+		mySQLPaymentPurposeGateway.deletePaymentPurposeByCode("");
+	}
+	
+	@Test
+	public void givenPaymentPurposeGateway_CallingUpdatePurposeName_PassingExistingPurposeCodeWithNewPurposeName_ShouldUpdateName() {
+		mySQLPaymentPurposeGateway.updatePaymentPurposeName("GHAD","Ahmadburghol");
+		PaymentPurposeDataStructure paymentPurposeDataStructure = mySQLPaymentPurposeGateway.loadPaymentPurposeByCode("GHAD");
+		assertEquals("Ahmadburghol", paymentPurposeDataStructure.getName());
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.NoneExistingPaymentPurposeException.class)
+	public void givenPaymentPurposeGateway_CallingUpdatePurposeName_PassingNoneExistingPurposeCodeWithNewPurposeName_ShouldThrowNoneExistingPaymentPurpose() {
+		mySQLPaymentPurposeGateway.updatePaymentPurposeName("AHM","Ahmadburghol");
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.NullPaymentPurposeCodeException.class)
+	public void givenPaymentPurposeGateway_CallingUpdatePurposeName_PassingNullPurposeCodeWithNewPurposeName_ShouldThrowNullPaymentPurposeCode() {
+		mySQLPaymentPurposeGateway.updatePaymentPurposeName(null,"Ahmadburghol");
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.EmptyPaymentPurposeCodeException.class)
+	public void givenPaymentPurposeGateway_CallingUpdatePurposeName_PassingEmptyPurposeCodeWithNewPurposeName_ShouldThrowEmptyPaymentPurposeCode() {
+		mySQLPaymentPurposeGateway.updatePaymentPurposeName("","Ahmadburghol");
+	}
+	
+	@Test(expected = MySQLPaymentPurposeGateway.NullPaymentPurposeNameException.class)
+	public void givenPaymentPurposeGateway_CallingUpdatePurposeName_PassingNullPurposeNameWithNewPurposeName_ShouldThrowNullPaymentPurposeName() {
+		mySQLPaymentPurposeGateway.updatePaymentPurposeName("AHM",null);
 	}
 
 }
